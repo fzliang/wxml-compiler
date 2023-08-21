b3c16fe644163fe3f459c7ce532ae261dfc48562
fzliang <cnfzliang@gmail.com>
2023-08-22 00:00:44 +0800

    feat: parse_tmpl

--- src/js_bindings.rs
+++ src/js_bindings.rs
@@ -41,3 +41,13 @@ impl TmplGroup {
         Ok(())
     }
 }
+
+#[wasm_bindgen(js_name="parseTmpl")]
+pub fn parse_tmpl(tmpl_str: &str) -> Result<String, JsError> {
+    let tmpl = crate::parse_tmpl(tmpl_str)?;
+
+    // todo 暂时将display输出
+    let ret_str = format!("{}", tmpl);
+
+    Ok(ret_str)
+}
